7b3b6f89f335caaf087d3b907d35f6f8a4b07be9
riicaampos <ricampos01@gmail.com>
2021-05-25 20:00:58 -0300

    Primeiro commit

---
+++ fundamentos/funcoes.js
@@ -0,0 +1,29 @@
+function soma(a,b){
+    console.log(a+b)
+}
+
+soma(1,5)
+
+
+const soma2 = function(a,b){
+    console.log(a+b)
+}
+
+soma2(10,5)
+
+
+const soma3 = function(a,b){
+    return a+b
+}
+
+console.log(soma3(20,3))
+
+
+const soma4 = (a,b) => {console.log(a+b)}
+soma4(100,3)
+
+const soma5 = (a,b) => console.log(a+b)
+soma5(9,3)
+
+const soma6 = (a,b) => a+b
+console.log(soma6(9,3))
